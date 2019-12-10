e4d51ee4f9c433fe56130a33b822e557ad659b89
Corentin Cadiou <contact@cphyc.me>
2019-12-10 18:39:25 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ cython_extensions = [
 
 setup(
     name='cython_fortran_file',
-    version='0.0.8',
+    version='0.0.9',
     description='An efficient package to read fortran-record files in Python.',
     long_description=readme,
     classifiers=[
