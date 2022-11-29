f2620e0eb0fdab55161ffb5a6338ac94ac2842e9
Corentin Cadiou <corentin.cadiou@cphyc.me>
2022-11-29 10:30:43 +0100

    Migrate to Ruff

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
+import numpy as np
+from Cython.Build import cythonize
 from setuptools import setup
 from setuptools.extension import Extension
 
-try:
-    import cython  # noqa
-    import numpy as np
-    from Cython.Build import cythonize
-
-    include_dirs = [np.get_include(), "cython_fortran_file"]
-except ImportError:
-    raise ImportError(
-        """Could not import cython or numpy. Building this package from source requires
-cython and numpy to be installed. Please install these packages using
-the appropriate package manager for your python environment."""
-    )
+include_dirs = [np.get_include(), "cython_fortran_file"]
 
 cython_extensions = [
     Extension(
